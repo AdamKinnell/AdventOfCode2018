2dae0eee5c5f8d84f81379e00e79e7b2cabd5c6d
Adam Kinnell <adamkinnell13@gmail.com>
2018-12-31 14:09:00 +1100

    Day4Part2: Complete

---
+++ src/bin/day4_part2.rs
@@ -0,0 +1,163 @@
+#[macro_use] mod common;
+use self::common::*;
+
+use itertools::Itertools;
+use std::collections::HashMap;
+
+// Types //////////////////////////////////////////////////////////////////////
+
+enum Event {
+    ShiftChangeTo(i32),
+    WakeUpAt(i32),
+    SleepAt(i32),
+}
+
+impl Event {
+    fn parse(line: &String) -> Event {
+        let parts= line
+            .replace(|c| "[]:#".contains(c)," ")
+            .split_whitespace()
+            .map(String::from)
+            .collect::<Vec<String>>();
+
+        if let [_date, _hour, minute, word_1, word_2] = &parts[0..5] {
+            match word_1.as_ref() {
+                "Guard" => {
+                    let id: i32 = word_2.parse().unwrap();
+                    Event::ShiftChangeTo(id)
+                },
+                "wakes" => {
+                    let minute = minute.parse().unwrap();
+                    Event::WakeUpAt(minute)
+                },
+                "falls" => {
+                    let minute = minute.parse().unwrap();
+                    Event::SleepAt(minute)
+                },
+                _ => {
+                    panic!("Unknown event")
+                }
+            }
+        } else {
+            panic!("Unable to parse event")
+        }
+    }
+}
+struct Shift {
+    guard: i32,
+    events: Vec<Event> // Must be ordered and non-overlapping
+}
+
+impl Shift {
+    fn is_asleep_at(&self, min: i32) -> bool {
+        let mut is_asleep = false;
+        for event in self.events.iter() {
+            match event {
+                Event::WakeUpAt(e_min) if is_asleep => {
+                    if min < *e_min { return true } // Asleep before wake event
+                    is_asleep = false;
+                },
+                Event::SleepAt(e_min) if !is_asleep => {
+                    if min < *e_min { return false } // Awake before sleep event
+                    is_asleep = true;
+                },
+                _ => panic!(),
+            }
+        }
+
+        // State after final event
+        return is_asleep;
+    }
+}
+
+// Functions //////////////////////////////////////////////////////////////////
+
+fn create_shifts<I>(events: I) -> Vec<Shift>
+    where I : Iterator<Item=Event>
+{
+    let mut shifts = Vec::new();
+
+    for event in events {
+        match event {
+            Event::ShiftChangeTo(id) => {
+                shifts.push(Shift { guard: id, events: Vec::new() });
+            },
+            other => {
+                let this_shift = shifts.last_mut().unwrap();
+                this_shift.events.push(other);
+            }
+        }
+    }
+
+    return shifts
+}
+
+fn find_most_frequently_asleep(shifts: &Vec<Shift>) -> (i32, i32) {
+
+    // Sum individual minutes asleep for each guard
+    let mut sleep_totals = HashMap::new();
+    for shift in shifts {
+        let entry = sleep_totals.entry(shift.guard).or_insert([0; 60]);
+        for min in 0..60 {
+            if shift.is_asleep_at(min) { entry[min as usize] += 1 }
+        }
+    }
+
+    // Find guard most frequently asleep on the same minute
+    let mut best_guard = 0;
+    let mut best_min = 0;
+    let mut best_min_count = 0;
+    for (&guard, mins) in sleep_totals.iter() {
+
+        // Find minute most asleep for this guard
+        let (this_min, &this_min_count) = mins.iter()
+            .enumerate()
+            .max_by(|(_,a),(_,b)| {
+                a.cmp(b)
+            })
+            .unwrap();
+
+        if this_min_count > best_min_count {
+            best_guard = guard;
+            best_min = this_min;
+            best_min_count = this_min_count;
+        }
+    }
+
+    (best_guard, best_min as i32)
+}
+
+fn solve(lines: &Vec<String>) -> (i32, i32) {
+
+    let events = lines.iter()
+        .sorted()
+        .map(Event::parse);
+
+    let shifts = create_shifts(events);
+    let (guard, minute) = find_most_frequently_asleep(&shifts);
+
+    (guard, minute)
+}
+
+// Entry Point ////////////////////////////////////////////////////////////////
+
+/*
+ Guard: 2389
+ Minute: 49
+ Answer: 117061
+*/
+run_without_benchmark!("day4", |lines: &Vec<String>| {
+    let (guard, minute) = solve(lines);
+    println!("\nGuard: {}", guard);
+    println!("Minute: {}", minute);
+    println!("Answer: {}\n", guard * minute);
+});
+
+/*
+ Timings:
+    DEBUG: ~27.9ms
+    RELEASE: ~1.76ms
+*/
+//run_with_benchmark!("day4", |lines| {
+//    solve(lines);
+//});
