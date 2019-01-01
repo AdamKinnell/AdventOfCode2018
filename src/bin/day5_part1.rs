02fe02c98f37723c97ef6ef5d6f820eebed2d67c
Adam Kinnell <adamkinnell13@gmail.com>
2019-01-01 23:02:42 +1100

    Revert "Day5Part1: Modify reaction code to be slightly more functional."
    This reverts commit 5984b257

--- src/bin/day5_part1.rs
+++ src/bin/day5_part1.rs
@@ -1,8 +1,6 @@
 #[macro_use] mod common;
 use self::common::*;
 
-use itertools::Itertools;
-
 // Functions //////////////////////////////////////////////////////////////////
 
 /*
@@ -12,53 +10,45 @@ fn can_react(a: char, b: char) -> bool {
     (a as i32 - b as i32).abs() == 32
 }
 
-/*
- Process polymer reactions until inert.
-*/
-fn react(polymer: &String) -> String {
-    let capacity = polymer.len();
+fn solve(polymer: &String) -> usize {
+
     let mut polymer= polymer.clone()
         .chars()
         .collect::<Vec<char>>();
+    let mut polymer_temp = Vec::<char>::with_capacity(polymer.len());
 
-    // Perform multiple passes to scan for reactions
+    // Process reactions until inert
     loop {
-        let mut iter = polymer.iter()
-            .chain(Some(' ').iter())
-            .tuple_windows();
-
-        let mut result = Vec::with_capacity(capacity);
-        while let Some((&a,&b)) = iter.next() {
-            if can_react(a,b) {
-                let _ = iter.next();
+        // Perform one scan for reactions
+        let mut i = 0;
+        while i < polymer.len() {
+            if i+1 < polymer.len() && can_react(polymer[i], polymer[i+1]) {
+                i += 2;
             } else {
-                result.push(a);
+                polymer_temp.push(polymer[i]);
+                i += 1;
             }
         }
 
-        if polymer.len() == result.len() {
-            break; // No more reactions in polymer
+        if polymer.len() == polymer_temp.len() {
+            // No more reactions in polymer
+            break;
         } else {
-            polymer = result; // Prepare for next scan
+            // Prepare for next scan
+            std::mem::swap(&mut polymer, &mut polymer_temp);
+            polymer_temp.clear();
         }
     }
 
-    polymer.into_iter().collect()
-}
-
-/*
- Find length of polymer after all interactions have been resolved.
-*/
-fn solve(polymer: &String) -> usize {
-    react(polymer).len()
+    polymer.len()
 }
 
 // Entry Point ////////////////////////////////////////////////////////////////
 
 /*
  Timings:
-    DEBUG: ~75.7ms
-    RELEASE: ~1.76ms
+    DEBUG: ~111ms
+    RELEASE: ~1.4ms
 */
 run!{
     input = "day5",
